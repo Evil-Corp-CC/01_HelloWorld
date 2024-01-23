924b6b41b7fc927c6d4ade519fb3676669625005
David <paulussen86@gmail.com>
2024-01-23 12:46:27 +0100

    update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,11 +2,13 @@ package com.cc.java;
 
 public class App 
 {
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello world");
+    
     }
     
+    
 }
+
 
 
